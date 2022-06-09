773abd928a3377d5a064cc5d2e9dab9223aa204d
James Craig <Jamescraig@MacBook-Pro-2.local>
2022-06-09 12:38:59 -0700

    initial commit

---
+++ worksheet.py
@@ -0,0 +1,19 @@
+age = '25'
+
+print('I am {} years old' .format(age))
+
+name = input("James")
+
+last = input("Craig")
+
+full = (name + last)
+
+print(full)
+
+print('My first name is {first}, and my last name is {last}, which means my full name is {full}')
+
+Fahrenheit_temperature = 90
+
+Celsius = 32.2
+
+"90 degrees Fahrenheit is 32.2 degrees Celsius"
